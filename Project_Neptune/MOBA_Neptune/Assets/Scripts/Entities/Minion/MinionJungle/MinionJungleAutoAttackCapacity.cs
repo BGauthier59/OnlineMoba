11f718d23e156c42934c9e12d93a3b9e736e4151
Arcaane <a.cord@rubika-edu.com>
2023-01-06 16:24:45 +0100

    MinionJungle Module v2

--- Project_Neptune/MOBA_Neptune/Assets/Scripts/Entities/Minion/MinionJungle/MinionJungleAutoAttackCapacity.cs
+++ Project_Neptune/MOBA_Neptune/Assets/Scripts/Entities/Minion/MinionJungle/MinionJungleAutoAttackCapacity.cs
@@ -1,38 +1,45 @@
 using System;
 using Entities;
 using Entities.Champion;
-using GameStates;
+using Entities.Minion.MinionJungle;
 using JetBrains.Annotations;
 using Photon.Pun;
 using UnityEngine;
 
 public class MinionJungleAutoAttackCapacity : NewActiveCapacity
 {
-    public float damage;
+    [Space] [Header("Capacity Stats")]
+    [SerializeField] private int capacityDamage;
     [SerializeField] private Vector3 casterPos;
     [SerializeField] private Vector3 targetPos;
+
+    private void Start()
+    {
+        if (!PhotonNetwork.IsMasterClient) return;
+        capacityDamage = GetComponent<MinionJungle>().autoAttackDamage;
+    }
 
     public override void RequestCastCapacity(int[] targetedEntities, Vector3[] targetedPositions)
     {
         photonView.RPC("CastMinionJungleAutoAttackCapacityRPC", RpcTarget.MasterClient, targetedEntities, targetedPositions);
     }
 
-    [PunRPC]
+    [PunRPC] [UsedImplicitly]
     public void CastMinionJungleAutoAttackCapacityRPC(int[] targetedEntities, Vector3[] targetedPositions)
     {
-        photonView.RPC("SyncDataJungleAutoAttackCapacityRPC", RpcTarget.All, targetedEntities, targetedPositions);
+        photonView.RPC("SyncDataJungleAutoAttackCapacityRPC", RpcTarget.All);
         
         foreach (var c in targetedEntities)
         {
             var damageable = EntityCollectionManager.GetEntityByIndex(c).GetComponent<IDamageable>();
-            damageable?.DecreaseCurrentHpRPC(damage, caster.entityIndex);
+            damageable?.DecreaseCurrentHpRPC(capacityDamage, caster.entityIndex);
             Debug.LogFormat($"{caster.name} attacked {EntityCollectionManager.GetEntityByIndex(c)} !");
             Debug.LogFormat($"{EntityCollectionManager.GetEntityByIndex(c)} current health : {EntityCollectionManager.GetEntityByIndex(c).GetComponent<Champion>().currentHp}");
         }
     }
 
-    [PunRPC]
-    public void SyncDataJungleAutoAttackCapacityRPC(int[] targetedEntities, Vector3[] targetedPositions)
+    [PunRPC] [UsedImplicitly]
+    public void SyncDataJungleAutoAttackCapacityRPC()
     {
         caster = GetComponent<Entity>();
     }
